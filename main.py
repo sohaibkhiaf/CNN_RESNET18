df2d77376306c2e20081eed8218ded7b64c3553e
Sohaib Khiaf <sohaibkhiaf@gmail.com>
2026-09-12 18:51:02 +0100

    Tiny Vgg removed

--- main.py
+++ main.py
@@ -103,61 +103,6 @@ print(f"Batch label shape: {y_batch.shape}")
 print("\n\n")
 
 
-# Tiny VGG architecture #################################
-class TinyVGG(nn.Module):
-  def __init__(self,
-               input_shape: int = 3,
-               hidden_units: int =20,
-               output_shape: int= len(class_names)):
-    super().__init__()
-    self.conv_block_1 = nn.Sequential(
-      nn.Conv2d(in_channels=input_shape,
-                out_channels=hidden_units,
-                kernel_size=3,
-                stride=1,
-                padding=1),
-      nn.ReLU(),
-      nn.Conv2d(in_channels=hidden_units,
-                out_channels=hidden_units,
-                kernel_size=3,
-                stride=1,
-                padding=1),
-      nn.ReLU(),
-      nn.MaxPool2d(kernel_size=2)
-    )
-
-    self.conv_block_2 = nn.Sequential(
-      nn.Conv2d(in_channels=hidden_units,
-                out_channels=hidden_units,
-                kernel_size=3,
-                stride=1,
-                padding=1),
-      nn.ReLU(),
-      nn.Conv2d(in_channels=hidden_units,
-                out_channels=hidden_units,
-                kernel_size=3,
-                stride=1,
-                padding=1),
-      nn.ReLU(),
-      nn.MaxPool2d(kernel_size=2)
-    )
-
-    self.classifier = nn.Sequential(
-      nn.Flatten(),
-      nn.Linear(in_features=hidden_units* 61* 61,
-                out_features=output_shape)
-    )
-
-  def forward(self, x):
-    x= self.conv_block_1(x)
-    # print(f"Output shape of conv_block_1: {x.shape}")
-    x= self.conv_block_2(x)
-    # print(f"Output shape of conv_block_2: {x.shape}")
-    x= self.classifier(x)
-    # print(f"Output shape of classifier: {x.shape}")
-    return x
-
-
 # residual block ===========================
 class BasicBlock(nn.Module):
   def __init__(self, in_channels, out_channels, stride=1):
